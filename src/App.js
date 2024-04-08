5c164557a304bf64fb57ab7b9f873b493a22187c
_Artyom_ <>
2024-04-08 10:45:27 +0300

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import './App.css';
 
+Modal.setAppElement('#root');
+
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App">
+        <button className="open-modal-button" onClick={() => setModalIsOpen(true)}>Открыть модальное окно</button>
+        <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={() => setModalIsOpen(false)}
+            className="modal-content"
+            overlayClassName="modal-overlay"
+            contentLabel="Простое модальное окно"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <h1>hello world</h1>
+            <h2>manera hahah</h2>
+            <h3>krytit mir</h3>
+          <button className="close-modal-button" onClick={() => setModalIsOpen(false)}>Закрыть модальное окно</button>
+        </Modal>
+      </div>
   );
 }
 
