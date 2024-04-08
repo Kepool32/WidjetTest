9e4c1b8217e1833bfe35bcf1f003fc0642957b08
_Artyom_ <>
2024-04-08 11:19:22 +0300

    ADD MODAL

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React, { useState } from 'react';
-import Modal from 'react-modal';
 import './App.css';
 
-Modal.setAppElement('#root');
-
 function App() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  return (
-      <div className="App">
-        <button className="open-modal-button" onClick={() => setModalIsOpen(true)}>Открыть модальное окно</button>
-        <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={() => setModalIsOpen(false)}
-            className="modal-content"
-            overlayClassName="modal-overlay"
-            contentLabel="Простое модальное окно"
-        >
-            <h1>hello world</h1>
-            <h2>manera hahah</h2>
-            <h3>krytit mir</h3>
-          <button className="close-modal-button" onClick={() => setModalIsOpen(false)}>Закрыть модальное окно</button>
-        </Modal>
-      </div>
-  );
+    return (
+        <div className="App">
+            <button className="open-modal-button" onClick={() => setModalIsOpen(true)}>Открыть модальное окно</button>
+            {modalIsOpen && (
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <h1>Простое модальное окно</h1>
+                        <p>Содержимое вашего модального окна...</p>
+                        <button className="close-modal-button" onClick={() => setModalIsOpen(false)}>Закрыть модальное окно</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
